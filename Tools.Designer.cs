322a1fde42fcfa5187fcb6e21739af770eaae7c8
Jayson Melecio <jaysonmelecio90@gmail.com>
2025-08-03 20:30:13 +0800

    Updated

--- Tools.Designer.cs
+++ Tools.Designer.cs
@@ -29,76 +29,76 @@ namespace OneLineParagraph
             // 
             // btnConvert
             // 
-            btnConvert.Anchor = System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right;
-            btnConvert.Location = new System.Drawing.Point(12, 120);
+            btnConvert.Anchor = System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right;
+            btnConvert.Location = new Point(12, 270);
             btnConvert.Name = "btnConvert";
-            btnConvert.Size = new System.Drawing.Size(563, 30);
+            btnConvert.Size = new Size(616, 30);
             btnConvert.TabIndex = 1;
             btnConvert.Text = "Convert to One Line";
             btnConvert.Click += BtnConvert_Click;
             // 
             // btnScramble
             // 
-            btnScramble.Anchor = System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right;
-            btnScramble.Location = new System.Drawing.Point(12, 156);
+            btnScramble.Anchor = System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right;
+            btnScramble.Location = new Point(12, 306);
             btnScramble.Name = "btnScramble";
-            btnScramble.Size = new System.Drawing.Size(563, 30);
+            btnScramble.Size = new Size(616, 30);
             btnScramble.TabIndex = 1;
             btnScramble.Text = "ScrambledTextGenerator";
             btnScramble.Click += BtnScramble_Click;
             // 
             // btnReplace
             // 
-            btnReplace.Anchor = System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right;
-            btnReplace.Location = new System.Drawing.Point(308, 192);
+            btnReplace.Anchor = System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right;
+            btnReplace.Location = new Point(361, 342);
             btnReplace.Name = "btnReplace";
-            btnReplace.Size = new System.Drawing.Size(161, 30);
+            btnReplace.Size = new Size(161, 30);
             btnReplace.TabIndex = 1;
             btnReplace.Text = "Replace";
             btnReplace.Click += BtnReplace_Click;
             // 
             // txtFind
             // 
-            txtFind.Anchor = System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right;
-            txtFind.Location = new System.Drawing.Point(12, 194);
+            txtFind.Anchor = System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right;
+            txtFind.Location = new Point(12, 344);
             txtFind.Multiline = true;
             txtFind.Name = "txtFind";
-            txtFind.Size = new System.Drawing.Size(290, 26);
+            txtFind.Size = new Size(343, 26);
             txtFind.TabIndex = 3;
             // 
             // button1
             // 
-            button1.Anchor = System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right;
-            button1.Location = new System.Drawing.Point(475, 192);
+            button1.Anchor = System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right;
+            button1.Location = new Point(528, 342);
             button1.Name = "button1";
-            button1.Size = new System.Drawing.Size(96, 30);
+            button1.Size = new Size(96, 30);
             button1.TabIndex = 1;
             button1.Text = "Copy";
             button1.Click += BtnCopyResult;
             // 
             // txtInput
             // 
-            txtInput.Anchor = System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right;
-            txtInput.Location = new System.Drawing.Point(12, 12);
+            txtInput.Anchor = System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right;
+            txtInput.Location = new Point(12, 12);
             txtInput.Name = "txtInput";
-            txtInput.Size = new System.Drawing.Size(563, 104);
+            txtInput.Size = new Size(616, 254);
             txtInput.TabIndex = 4;
             txtInput.Text = "";
             // 
             // txtOutput
             // 
-            txtOutput.Anchor = System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right;
-            txtOutput.Location = new System.Drawing.Point(12, 231);
+            txtOutput.Anchor = System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right;
+            txtOutput.Location = new Point(12, 381);
             txtOutput.Multiline = true;
             txtOutput.Name = "txtOutput";
             txtOutput.ReadOnly = true;
             txtOutput.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            txtOutput.Size = new System.Drawing.Size(559, 149);
+            txtOutput.Size = new Size(612, 149);
             txtOutput.TabIndex = 2;
             // 
             // Tools
             // 
-            ClientSize = new System.Drawing.Size(583, 392);
+            ClientSize = new Size(636, 542);
             Controls.Add(txtInput);
             Controls.Add(txtFind);
             Controls.Add(button1);
@@ -106,10 +106,10 @@ namespace OneLineParagraph
             Controls.Add(btnScramble);
             Controls.Add(btnConvert);
             Controls.Add(txtOutput);
-            Icon = (System.Drawing.Icon)resources.GetObject("$this.Icon");
+            Icon = (Icon)resources.GetObject("$this.Icon");
             Name = "Tools";
             Text = "Paragraph to One Line";
-            Load += this.Tools_Load;
+            Load += Tools_Load;
             ResumeLayout(false);
             PerformLayout();
         }
